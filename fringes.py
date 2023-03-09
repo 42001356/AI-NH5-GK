2961ee57aa23a5330e82c6f40f2b9c8a1495f1c3
Quang Lam <125286142+42001356@users.noreply.github.com>
2023-03-09 18:15:50 +0700

    update Stack, Queue, Priority Queue

--- fringes.py
+++ fringes.py
@@ -1,84 +1,80 @@
-#Stack
+# Stack
 class Stack:
     def __init__(self) -> None:
         self.items = list()
-    
+
     def empty(self):
         return len(self.items) == 0
-    
-    def push(self, item):
-        self.items.append(item)
-    
+
+    def push(self, state: tuple):
+        self.items.append(state)
+
     def pop(self):
         return self.items.pop()
-    
+
     def peek(self):
         return self.items[-1]
-    
+
     def size(self):
         return len(self.items)
-    
-    def contains(self, item):
-        return item in self.items
-    
+
+    def contains(self, state: tuple):
+        return state in self.items
+
     def __str__(self) -> str:
         return str(self.items)
 
-#Queue
+
+# Queue
 class Queue:
     def __init__(self) -> None:
         self.items = list()
 
     def empty(self):
         return len(self.items) == 0
-    
-    def enqueue(self, item):
-        self.items.append(item)
-    
+
+    def enqueue(self, state: tuple):
+        self.items.append(state)
+
     def dequeue(self):
         return self.items.pop(0)
-    
+
     def front(self):
         return self.items[0]
-    
+
     def size(self):
         return len(self.items)
-    
-    def contains(self, item):
-        return  item in self.items
-    
+
+    def contains(self, state: tuple):
+        return state in self.items
+
     def __str__(self) -> str:
         return str(self.items)
 
-#Priority Queue    
+
+# Priority Queue
 class PriorityQueue:
     def __init__(self) -> None:
         self.items = list()
-    
+
     def empty(self):
         return len(self.items) == 0
-    
-    def enqueue(self, item):
-        self.items.append(item)
+
+    def enqueue(self, state: tuple):
+        self.items.append(state)
         self.items.sort()
+
+    def dequeue(self):
+        return self.items.pop(0)
     
-    def getPriority(self, vertice):
-        for w,v in self.items:
-            if v == vertice:
-                return w
-        raise 'Key Error'
-    
-    def updatePriority(self, v):
-        p_w = self.getPriority(v)
-        self.items.remove((p_w, v))
+    def getPriority(self):
+        return self.items[0]
 
     def size(self):
         return len(self.items)
-    
-    def contains(self, item):
-        return item in self.items
-    
+
+    def contains(self, state: tuple):
+        return state in self.items
+
     def __str__(self) -> str:
         return str(self.items)
-    
-
