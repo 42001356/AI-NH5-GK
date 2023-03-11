7fce9e4c8cb371ec03669fd9902036d82fe07d85
Quang Lam <125286142+42001356@users.noreply.github.com>
2023-03-11 13:54:34 +0700

    update MultiFoodSearchProblem

--- problems.py
+++ problems.py
@@ -155,74 +155,12 @@ class SingleFoodSearchProblem:
                 os.system('cls')
 
 # MultiFoodSearchProblem
-class MultiFoodSearchProblem:
-    def __init__(self) -> None:
-        self.state = tuple()
-        self.node = list()
-        self.initial_state = tuple()
-
-    def getState(self):
-        return self.state
-
-    def getNode(self):
-        return self.node
-
-    def getInitialState(self):
-        return self.initial_state
-
-    def setState(self, state: tuple):
-        self.state = state
-
-    def setNode(self, node: list):
-        self.node = node
-
-    def setInitialState(self, initial_state: tuple):
-        self.initial_state = initial_state
-
+class MultiFoodSearchProblem(SingleFoodSearchProblem):
+    # @ Override
     def goalTest(self, state: tuple):
         return state[1].count('Stop') == (np.count_nonzero(state[3] == 3) + np.count_nonzero(state[3] == 5))
 
-    def pathCost(self, state: tuple):
-        if 'Stop' not in state[1]:
-            return len(state[1].split(","))
-        return len(state[1].split(",")) - 1
-
-    def readMaze(self, filename):
-        i = 0
-        number_of_cells = 0
-        if os.path.exists(filename):
-            with open(filename) as f:
-                for line in f:
-                    col = len(line)
-                    for char in line:
-                        number_of_cells += 1
-            col += 1
-            number_of_cells += 1
-            row = int(number_of_cells / col)
-            matrix = np.arange(number_of_cells)
-            matrix[-1] = 4
-            with open(filename) as f:
-                for line in f:
-                    for char in line:
-                        if char == ' ':
-                            matrix[i] = 1
-                        elif char == 'P':
-                            matrix[i] = 2
-                        elif char == '.':
-                            matrix[i] = 3
-                        elif char == '\n':
-                            matrix[i] = 4
-                        else:
-                            matrix[i] = 0
-                        i += 1
-            matrix = matrix.reshape(row, col)
-            for i in range(matrix.shape[0]):
-                for j in range(matrix.shape[1]):
-                    if matrix[i][j] == 2:
-                        self.initial_state = (0, 'Start', (i, j), matrix)
-                        self.state = self.initial_state
-                        return 0
-
+    # @ Override
     def successor(self, state: tuple):
         self.node = []
         self.state = state
@@ -263,53 +201,3 @@ class MultiFoodSearchProblem:
                 if state[3][i + 1][j] == 3:
                     state[3][i + 1][j] == 5
                     self.node.append((state[0] + 1, state[1] + ',Stop', (i + 1, j), state[3].copy()))
-
-    def printMaze(self, matrix) -> None:
-        matrix_text = ''
-        for i in range(matrix.shape[0]):
-            for j in range(matrix.shape[1]):
-                if matrix[i][j] == 0:
-                    matrix_text += '%'
-                if matrix[i][j] == 1:
-                    matrix_text += ' '
-                if matrix[i][j] == -1:
-                    matrix_text += colored('-', 'red')
-                if i == self.initial_state[2][0] and j == self.initial_state[2][1]:
-                    matrix_text += colored('P', 'blue', attrs=['reverse', 'bold'])
-                if matrix[i][j] == 3 or matrix[i][j] == 5:
-                    matrix_text += colored('.', 'green', attrs=['reverse', 'bold'])
-                if matrix[i][j] == 4:
-                    matrix_text += '\n'
-        
-        print(matrix_text)
-    
-    def animate(self, action) -> None:
-        action_matrix = self.initial_state[3].copy()
-        i = self.initial_state[2][0]
-        j = self.initial_state[2][1]
-        for state in action:
-            if state == 'W':
-                action_matrix[i][j-1] = -1
-                self.printMaze(action_matrix)
-                j = j - 1
-                enter = input()
-                os.system('cls')
-            if state == 'E':
-                action_matrix[i][j+1] = -1
-                self.printMaze(action_matrix)
-                j = j + 1
-                enter = input()
-                os.system('cls')
-
-            if state == 'N':
-                action_matrix[i-1][j] = -1
-                self.printMaze(action_matrix)
-                i = i - 1
-                enter = input()
-                os.system('cls')
-            if state == 'S':
-                action_matrix[i+1][j] = -1
-                self.printMaze(action_matrix)
-                i = i + 1
-                enter = input()
-                os.system('cls')
